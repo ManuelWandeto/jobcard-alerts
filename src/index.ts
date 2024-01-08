ce276dd2bd1b1a281b8f1aa45251dfc840670c77
Manuel Wandeto <immanuelmojo@hotmail.com>
2024-01-08 17:28:12 +0300

    Update index.ts
    change prompting to take jobcard statuses over 'hey'

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ const bot = createBot(
     process.env.ACCESS_TOKEN ?? '',
 );
 
-let records = new Map<string, {page: number, hasNextPage?: boolean, lastRequest: number}>();
+let records = new Map<string, {status: string, page: number, hasNextPage?: boolean, lastRequest: number}>();
+const statuses = ['CANCELLED','SUSPENDED','REPORTED','SCHEDULED','ONGOING','COMPLETED','OVERDUE']
 try {
     await bot.startExpressServer({
         webhookVerifyToken: process.env.WEBHOOK_VERIFICATION_TOKEN ?? ''
@@ -35,52 +36,51 @@ try {
                 await bot.sendMessage(msg.from, 'Unsupported message type')
                 return
             }
-            const text = msg.data?.text?.trim().toLowerCase()
+            const text: string = msg.data?.text?.trim().toLowerCase()
             let record = records.get(msg.from)
-            
-            if( text === "hey") {
-                if(record && moment(record.lastRequest).diff(Date.now(), 'hours') < 12) {
-                    const nextRequestTime = moment(record.lastRequest).add(12, 'hours').format('YYYY-MM-DD [at:] h:mm A')
-                    await bot.sendMessage(msg.from, `Your last request was less than 12hours ago, job reminders are available in 12 hour intervals, request again as from ${nextRequestTime}`)
-                    return
-                }
-                const res = await getJobcards(pool, 1)
-                if(!res) {
-                    await bot.sendText(msg.from, 'An error occured fetching jobcards, please try again \n')
-                    return
-                }
+            if(!record || moment(record.lastRequest).diff(Date.now(), 'hours') >= 24) {
                 await bot.sendText(msg.from, 'Hello from jobcards! \n')
-                await sendReports(res, bot, msg.from)
-                records.set(msg.from, {page: 1, hasNextPage: res?.hasNextPage, lastRequest: Date.now()})
+            }
+            if(statuses.includes(text.toUpperCase())) {
+                const res = await getJobcards(pool, text, 1)
+                await sendReports(res!, text, bot, msg.from)
+                records.set(msg.from, {status: text, page: 1, hasNextPage: res?.hasNextPage, lastRequest: Date.now()})
                 return
             }
             if( text === 'more') {
-                if(record && moment(record.lastRequest).diff(Date.now(), 'hours') < 12) {
+                if(record) {
                     if(record.hasNextPage) {
                         await bot.sendMessage(msg.from, 'Fetching...')
                         let currentPage = record.page
-                        const res = await getJobcards(pool, currentPage ? currentPage + 1 : 1)
+                        const res = await getJobcards(pool, record.status, currentPage ? currentPage + 1 : 1)
                         if(!res) {
                             await bot.sendText(msg.from, 'An error occured fetching jobcards, please try again \n')
                             return
                         }
-                        await sendReports(res, bot, msg.from)
-                        records.set(msg.from, {page: currentPage ? currentPage + 1 : 1, hasNextPage: res.hasNextPage, lastRequest: record.lastRequest})
+                        await sendReports(res, record.status, bot, msg.from)
+                        records.set(msg.from, {
+                            
+                            page: currentPage ? currentPage + 1 : 1, 
+                            hasNextPage: res.hasNextPage, 
+                            status: record.status,
+                            lastRequest: record.lastRequest
+                        })
                     } else {
-                        await bot.sendMessage(msg.from, 'No more jobs to show')
+                        await bot.sendMessage(msg.from, `No more ${record.status} jobs to show for now`)
                     }
                 } else {
-                    await bot.sendMessage(msg.from, 'Text "Hey" to get the daily job reminders for today')
+                    await bot.sendMessage(msg.from, `Text either ${statuses.join(', ')} to get the daily job reminders for today`)
                 }
                 return
             }
             if(record?.hasNextPage) {
                 await bot.sendMessage(msg.from, `Invalid! reply with 'more' for more jobs`)
             } else {
-                await bot.sendMessage(msg.from, `Invalid reply, however, no more jobs available at this time`)
+                await bot.sendMessage(msg.from, `Text either ${statuses.join(', ')} to get the daily job reminders for today`)
             }
             
         } catch (error) {
+            await bot.sendText(msg.from, 'An error occured fetching jobcards, please try again \n')
             userLog.error(error, 'Error serving user')
         }
 
@@ -91,9 +91,9 @@ try {
         .catch(e=>logger.error(e, 'Error sending message alert'))
 }
 
-async function sendReports(res: JobsResponse, bot: Bot, recipient: string) {
+async function sendReports(res: JobsResponse, status: string, bot: Bot, recipient: string) {
     try {
-        await bot.sendMessage(recipient, 'The following jobs are either overdue, scheduled, reported or unscheduled:')
+        await bot.sendMessage(recipient, `The following jobs are ${status.toUpperCase()}`)
         for (const job of res.jobcards) {
             await bot.sendMessage(recipient, createJobReport(job))
         }
